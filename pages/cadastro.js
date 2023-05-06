2c65a3c4024426c15c22d724c8fce18f79c20cac
Amadeu dos Anjos Barros <anjosbarrosclean@gmail.com>
2023-05-06 17:45:04 -0300

    :lipstick: feat: estilizando o formulário

--- pages/cadastro.js
+++ pages/cadastro.js
@@ -1,13 +1,21 @@
-import LoginCard from 'src/components/loginCard/loginCard'
-import styles from '../styles/Cadastro.module.css'
+import Link from 'next/link'
+import styles from '../styles/Login.module.css'
+import LoginCard from '../src/components/loginCard/loginCard'
+import Button from '../src/components/button/button'
+import Input from '../src/components/input/input'
 
 export default function CadastroPage() {
-    return (
-      <div className={styles.background}>      
-        <LoginCard title="Crie sua conta">
-          Cadastro
-          </LoginCard>
-      </div>
-    )
-  }
-  
+  return (
+    <div className={styles.background}>      
+      <LoginCard title="Crie sua conta">
+        <form className={styles.form}>
+          <Input type="text" placeholder="Seu nome" />
+          <Input type="email" placeholder="Seu e-mail" />
+          <Input type="password" placeholder="Sua senha" />
+          <Button>Login</Button>
+          <Link href="/login">Login</Link>
+        </form>
+      </LoginCard>
+    </div>
+  )
+}
--- pages/login.js
+++ pages/login.js
@@ -1,17 +1,19 @@
+import Link from 'next/link'
 import styles from '../styles/Login.module.css'
 import LoginCard from '../src/components/loginCard/loginCard'
 import Button from '../src/components/button/button'
 import Input from '../src/components/input/input'
 
-export default function LoginPage() {
+export default function CadastroPage() {
   return (
     <div className={styles.background}>      
-      <LoginCard title="Entre em sua conta">
+      <LoginCard title="Entrar na sua conta">
         <form className={styles.form}>
           <Input type="email" placeholder="Seu e-mail" />
           <Input type="password" placeholder="Sua senha" />
+          <Button>Login</Button>
+          <p>Não tem Login ?<Link href="/cadastro">Cadastre-se</Link></p>
         </form>
-        <Button>Login</Button>
       </LoginCard>
     </div>
   )
