c30eb1f625938fc816758c5ec91d306f07b85ea8
Amadeu dos Anjos Barros <anjosbarrosclean@gmail.com>
2023-05-05 21:40:02 -0300

    :lipstick: feat: estilizando o botao

--- pages/login.js
+++ pages/login.js
@@ -7,8 +7,10 @@ export default function LoginPage() {
   return (
     <div className={styles.background}>      
       <LoginCard title="Entre em sua conta">
-        <Input type="email" placeholder="Seu e-mail" />
-        <Input type="password" placeholder="Sua senha" />
+        <form className={styles.form}>
+          <Input type="email" placeholder="Seu e-mail" />
+          <Input type="password" placeholder="Sua senha" />
+        </form>
         <Button>Login</Button>
       </LoginCard>
     </div>
