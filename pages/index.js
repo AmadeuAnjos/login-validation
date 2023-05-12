3695256dd8669c092e2f26707c75e7410dad838d
Amadeu dos Anjos Barros <anjosbarrosclean@gmail.com>
2023-05-11 22:04:43 -0300

    :lipstick: feat: alterando formulário

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,31 @@
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import { verifica } from '../services/user'
+import { getCookie } from 'cookies-next'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <div className={styles.home}>      
-      <h1>Página 1</h1>
+      <h1>Página acessada com sucesso</h1>
     </div>
   )
 }
+
+export const getServerSideProps = async ({ req, res }) => {
+  try {
+    const token = getCookie('authorization', { req, res })
+    console.log(token)
+
+    return {
+      props: {}
+    }
+    
+  } catch (err) {
+    return {
+      props: {}
+    }
+  }
+}
+
