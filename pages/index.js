da2c7a9b5bb9f111a3867bdc57e06596babf86ea
Amadeu dos Anjos Barros <anjosbarrosclean@gmail.com>
2023-05-15 20:37:37 -0300

    :lipstick: feat: alterando detalhes do css

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,17 @@ import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import { verifica } from '../services/user'
 import { getCookie } from 'cookies-next'
+import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <div className={styles.home}>      
-      <h1>Página acessada com sucesso</h1>
+      <div className={styles.log}>
+        <h1>Página acessada com sucesso</h1>
+        <Link className={styles.paragraph} href="/login">Ir para tela de login</Link>
+      </div>
     </div>
   )
 }
